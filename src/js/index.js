ce22f83c6a3bf178b674df02b83d0b36658ab86a
Bryan <you@example.com>
2024-01-30 22:29:30 -0500

    changed wording + refactoring

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,6 @@
-// alert("oi");
 const botao = document.querySelector(".btn-plataforma");
 const elementoPlataformas = document.querySelector(".btn-plataforma .plataformas")
 
 botao.addEventListener("click", () => {
-    // toggle alterna o "ativo", se tiver e clicar, ele remove. Caso o contrario ele adiciona.
     elementoPlataformas.classList.toggle("ativo");
 })
